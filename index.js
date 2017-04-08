c5911c2093a53cdd30b4e923e6e3331ed8867e61
Pedro Sena <sena.pedro@gmail.com>
2017-04-08 16:38:32 -0300

    Handling no cards found

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ bot.command('start', (ctx) => {
 bot.command('card', (ctx) => {
   const cardName = ctx.message.text.split("/card")[1].trim()
   mtg.card.where({name: cardName, pageSize: 10}).then(cards => {
+    if ( cards.length == 0 ) {
+      ctx.reply(`Nao encontrei cartas com o nome ${cardName}`)
+      return
+    }
     const names = new Set(cards.map(c => { c.name }))
+    //Same card, different editions, get first one
     if (names.size == 1) {
       ctx.reply(cards[0].imageUrl)
       return
